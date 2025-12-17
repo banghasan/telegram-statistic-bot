a518e42020a4b8e55f84805be44774c71ebecb03
bangHasan <banghasan@gmail.com>
2025-12-17 16:10:32 +0700

    start command message

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,9 +1,18 @@
-import type { Bot, Context } from "gramio";
+import { Bot, InlineKeyboard, type Context } from "gramio";
 
 export function loadStartCommand(bot: Bot) {
-  bot.command("start", (context: Context) => {
+  bot.command("start", async (context: Context) => {
+    const me = await bot.api.getMe();
+    const keyboard = new InlineKeyboard().url(
+      "👥 Tambahkan ke Grup",
+      `https://t.me/${me.username}?startgroup=true`,
+    );
     return context.reply(
-      "Welcome! I'm a statistics bot. Add me to a group, and I'll start tracking user activity. Use /stats to view the stats web app!"
+      "Halo semuanya!\n\nKenalin nih, Saya adalah Bot Statistik yang bisa bantu pantau aktivitas grup. Biar botnya bisa bekerja maksimal dan kasih data yang akurat, jangan lupa tambahkan saya ke grup kita ya!\n\nSeru banget lho bisa lihat siapa yang paling rajin ngobrol di sini.\n\n- Perintah tersedia saat ini: `/stats`",
+      {
+        reply_markup: keyboard,
+        parse_mode: "markdown",
+      },
     );
   });
 }
